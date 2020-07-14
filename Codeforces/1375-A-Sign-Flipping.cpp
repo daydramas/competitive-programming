4b41d0a342e6ec7cdd4c92e8140314b34d68541a
Dong Liu <59348162+dongliu0426@users.noreply.github.com>
2020-07-14 14:42:29 -0700

    Create 1375-A-Sign-Flipping.cpp

---
+++ Codeforces/1375-A-Sign-Flipping.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// defines
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define f(i, a, b) for(ll i = a; i < b; i++)
+#define rf(i, a, b) for(ll i = a; i >= b; i--)
+#define w(a) while(a--)
+#define arrin(a,n) f(i,0,n) { cin >> a[i]; }
+#define arrin1(a,n) f(i,1,n+1) { cin >> a[i]; }
+#define arrout(a,n) f(i,0,n) { cout << a[i]<<" "; }
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ll gcd(ll a,ll b){ return (b==0) ? a : gcd(b,a%b); }
+ll lcm(ll a,ll b){ return (a * b) / gcd(a,b); }
+
+// structures
+
+// global variables
+
+// functions
+void solve() {
+  int n; cin >> n;
+  f(i,0,n) {
+    int a; cin >> a; 
+    if (i&1) {
+      if (a < 0) a *= -1;
+    } else {
+      if (a > 0) a *= -1;
+    }
+    cout << a <<" ";
+  }
+  cout <<"\n";
+}
+int main() {
+
+	// local variables
+
+	// input
+	fastio;
+  int t; cin >> t;
+  w(t) solve();
+	// main
+
+	// output
+
+	return 0;
+}
