7aaf7e2862e89fe35bb43f99a31ab74ed5063e92
Dong Liu <dongliu0426@gmail.com>
2020-08-31 10:24:52 -0700

    Finish more training problems

--- USACO/Training/6.2/calfflac/main.cpp
+++ USACO/Training/6.2/calfflac/main.cpp
@@ -54,21 +54,54 @@ void setIO(const string &PROB = "") {
 
 int n;
 string S;
+vector<int> idx;
+int line[20005];
 
 int main() {
     setIO("calfflac");
     
     string t;
-    while( getline(cin, t) ) S += t;
+    while( getline(cin, t) ){
+        S += t;
+        line[S.length()]=1;
+    }
     n = S.length();
-    string F;
-
-
-    for0(i, n) {
-        if ((S[i]>='a' && S[i]<='z') || (S[i]>='A' && S[i]<='Z')){
-            F += tolower(S[i]);
+    string N = "-";
+    for0(i, n){
+        char ch = S[i];
+        if (ch>='a'&&ch<='z') {
+            N += ch;
+            N += "-";
+            idx.pb(i);
+        }
+        if (ch>='A'&&ch<='Z') {
+            N += ch-'A'+'a'; // convert from uppercase to lowercase
+            N += "-";
+            idx.pb(i);
         }
     }
-    cout << F << endl;
+    vector<int> dp(N.length()+1);
+    for (int i = 1; i < N.length(); i++) {
+        dp[i]=0;
+        while (i - 1 - dp[i] >= 0 && i + 1 + dp[i] < N.length() && N[i - 1 - dp[i]] == N[i + 1 + dp[i]])
+         dp[i]++;
+    }
+    int len = 0, index = -1;
+    for (int i = 0; i < N.length(); i++) {
+        if (dp[i] > len) {
+            len = dp[i];
+            index = i;
+        }
+    }
+    int a = idx[(index - dp[index]) / 2];
+    int b = idx[(index + dp[index]) / 2 - 1];
+    cout << len << endl;
+    string S2 = S.substr(a, b - a + 1);
+    // int cnt=80;
+    for (int i=a; i<=b; i++) {
+        if (line[i]) cout <<"\n";
+        cout << S[i];
+    }
+    cout <<"\n";
     return 0;
 }
