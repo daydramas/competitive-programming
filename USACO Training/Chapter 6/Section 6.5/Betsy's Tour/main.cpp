d15c7d4b5e8437d9d83575074afa40973d00d4bc
Dong Liu <dongliu0426@gmail.com>
2020-10-10 16:34:07 -0700

    Finish ALL Training problems

---
+++ USACO Training/Chapter 6/Section 6.5/Betsy's Tour/main.cpp
@@ -0,0 +1,155 @@
+/*
+ ID: dongliu3
+ TASK: betsy
+ LANG: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Defines */
+
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i) 
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i) 
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) 
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)
+#define DBG(x) cout <<#x<<": "<<x<<endl;
+#define DBG2(x,y) cout <<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl;
+
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define sz(a) int((a).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+/* Typdefs */
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+/* IO */
+
+ifstream fin;
+ofstream fout;
+
+void setIO(const string &PROB = "") {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    if (PROB.length() != 0) {
+        ifstream infile(PROB + ".in");
+        if (infile.good()) {
+            fin.open(PROB + ".in");
+            fout.open(PROB + ".out");
+        }
+    }
+}
+
+/* ============================ */
+
+int n;
+bool v[10][10];
+
+int X[] = {0, 1, 0, -1};
+int Y[] = {1, 0, -1, 0};
+
+bool vl(int x, int y) { return ( x>=1 && x<=n && y>=1 && y<=n ); }
+bool vl2(int x, int y) { return !vl(x,y)||v[x][y]; }
+
+// bool de(int x, int y) {
+//     if (vl2(x+1,y) && vl2(x-1,y) && vl2(x,y+1) && vl2(x,y-1)) return false;
+//     // if (vl2(x+1,y+1) && vl2(x+1,y-1) && vl2(x+2,y)) { return false; }
+//     // if (vl2(x-1,y-1) && vl2(x-1,y+1) && vl2(x-2,y)) { return false; }
+//     // if (vl2(x-1,y+1) && vl2(x+1,y+1) && vl2(x,y+2)) { return false; }
+//     // if (vl2(x-1,y-1) && vl2(x+1,y-1) && vl2(x,y-2)) { return false; }
+//     return true;
+// }
+bool de(int x, int y) {
+    if ((!vl(x + 1, y) || v[x + 1][y]) && (!vl(x - 1, y) || v[x - 1][y])) {
+        if (vl(x, y - 1) && !v[x][y - 1] && vl(x, y + 1) && !v[x][y + 1])
+            return true;
+    }
+    if ((!vl(x, y - 1) || v[x][y - 1]) && (!vl(x, y + 1) || v[x][y + 1])) {
+        if (vl(x - 1, y) && !v[x - 1][y] && vl(x + 1, y) && !v[x + 1][y])
+            return true;
+    }
+    return false;
+}
+
+void pv() {
+    for1(i,n) {
+        for1(j,n) cout << v[i][j]<<" ";
+        cout <<"\n";
+    }
+    cout <<"\n";
+}
+
+int ccnt(int i, int j) {
+    int cnt=0;
+    for0(k, 4) {
+        cnt += (vl(i+X[k],j+Y[k]) && !v[i+X[k]][j+Y[k]]);
+    }
+    return cnt;
+}
+
+int dfs(int i, int j, int vis) {
+    if (i==n && j==1) return (vis==n*n);
+    if (vis==n*n) return 0;
+    if (de(i, j)) return 0;
+
+    int ans = 0;
+    int cnt=0, _i=-1;
+    for0(k, 4) {
+        int x1 = i+X[k], y1 = j+Y[k];
+        if (vl(x1, y1) && !v[x1][y1] && ccnt(x1, y1) == 1) {
+            if (x1 == n && y1 == 1) continue;
+            cnt++, _i = k;
+        }
+    }
+    if (cnt == 1) {
+        int x=(i+X[_i]), y=(j+Y[_i]);
+        v[x][y]=true;
+        ans += dfs(i+X[_i], j+Y[_i], vis+1);
+        v[x][y]=false;
+    } else {
+        for0(k, 4) {
+            int x=(i+X[k]), y=(j+Y[k]);
+            if (vl(x, y) && !v[x][y]) {
+                v[x][y]=true;
+                ans += dfs(x, y, vis+1);
+                v[x][y]=false;
+            }
+        }
+    }
+    return ans;
+    // for0(k, 4) {
+    //     int x=i+X[k], y=j+Y[k];
+    //     if (vl(x, y) && !v[x][y]) {
+            
+    //         v[x][y] = true;
+    //         cnt += dfs(x, y, vis+1);
+    //         v[x][y] = false;
+    //     }
+    // }
+    return cnt;
+}
+
+int main() {
+    cout <<"\n+------ Starting\n"; clock_t startT = clock();
+    setIO("betsy");
+
+    fin >> n;
+    memset(v, false, sizeof(v));
+    v[1][1]=true;
+    fout << dfs(1, 1, 1) << "\n";
+    cout <<"+------ Ending | Time taken: "<<((float)(clock()-startT)/CLOCKS_PER_SEC)<<" seconds \n\n";
+    return 0;
+}
---
+++ USACO Training/Chapter 6/Section 6.5/Closed Fences/main.cpp
@@ -0,0 +1,140 @@
+/*
+ ID: dongliu3
+ TASK: fence4
+ LANG: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Defines */
+
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i) 
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i) 
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) 
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) 
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)
+#define DBG(x) cout <<#x<<": "<<x<<endl;
+#define DBG2(x,y) cout <<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl;
+
+#define pb push_back
+#define x first
+#define y second
+#define mp make_pair
+#define sz(a) int((a).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+
+/* Typdefs */
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<double, double> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+/* IO */
+
+ifstream fin;
+ofstream fout;
+
+void setIO(const string &PROB = "") {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    if (PROB.length() != 0) {
+        ifstream infile(PROB + ".in");
+        if (infile.good()) {
+            fin.open(PROB + ".in");
+            fout.open(PROB + ".out");
+        }
+    }
+}
+
+/* ============================ */
+
+struct line {
+    ii p1, p2;
+    line(ii p1, ii p2) : p1(p1), p2(p2) {}
+};
+
+int n;
+ii fj;
+ii pnt[205];
+bool ok[205];
+vi ans; 
+
+int ccw(ii p0, ii p1, ii p2) {
+	double dx1, dx2, dy1, dy2;
+	dx1 = p1.x - p0.x; dy1 = p1.y - p0.y;
+	dx2 = p2.x - p0.x; dy2 = p2.y - p0.y;
+	if (dx1 * dy2 > dy1 * dx2) return +1;
+	if (dx1 * dy2 < dy1 * dx2) return -1;
+	if ((dx1 * dx2 < 0) || (dy1 * dy2 < 0)) return -1;
+	if ((dx1 * dx1 + dy1 * dy1) < (dx2 * dx2 + dy2 * dy2)) return +1;
+	return 0;
+}
+int intersect(struct line l1, struct line l2) {
+	return ((ccw(l1.p1, l1.p2, l2.p1) * ccw(l1.p1, l1.p2, l2.p2)) <= 0)
+			&& ((ccw(l2.p1, l2.p2, l1.p1) * ccw(l2.p1, l2.p2, l1.p2)) <= 0);
+}
+int intersect_(struct line l1, struct line l2) {
+	return ((ccw(l1.p1, l1.p2, l2.p1) * ccw(l1.p1, l1.p2, l2.p2)) < 0)
+			&& ((ccw(l2.p1, l2.p2, l1.p1) * ccw(l2.p1, l2.p2, l1.p2)) < 0);
+}
+
+bool valid() {
+    for0(i, n) for0(j, n) if (j!=i && (i+1)%n!=j && (j+1)%n!=i){
+        if (intersect(line(pnt[i], pnt[(i+1)%n]), line(pnt[j], pnt[(j+1)%n]))) {
+            return false;
+        }
+    }
+    return true;
+}
+bool comp(int i, int j) {
+    ii a = {i, (i+1)%n};
+    ii b = {j, (j+1)%n};
+    if (a.y==0) swap(a.x, a.y);
+    if (b.y==0) swap(b.x, b.y);
+    if (a.y!=b.y) return a.y<b.y;
+    return a.x<b.x;
+}
+int main() {
+    setIO("fence4");
+    
+    fin >> n;
+    fin >> fj.x >> fj.y;
+    for0(i, n) fin >> pnt[i].x >> pnt[i].y;
+
+    if (!valid()) { cout << "NOFENCE\n"; return 0; }
+
+    memset(ok, true, sizeof(ok));
+    for0(i, n) {
+        ii t = { pnt[i].x, pnt[i].y };
+        ii d = { (pnt[(i+1)%n].x-pnt[i].x)/1000,
+                 (pnt[(i+1)%n].y-pnt[i].y)/1000 };
+        for0(it,999) {
+            t = { t.x+d.x, t.y+d.y };
+            bool ok = true;
+            for0(j, n) if(j!=i) {
+                if (intersect(line(fj, t), line(pnt[j], pnt[(j+1)%n]))) {
+                    // if (i==0) cout<<pnt[j].x<<" "<<pnt[j].y<<" | "<<pnt[(j+1)%n].x<<" "<<pnt[(j+1)%n].y<<"\n";
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) { ans.pb(i); break; }
+        }
+    }
+    fout <<ans.size()<<"\n";
+    sort(all(ans), comp);
+    for0(j, sz(ans)) {
+        int i=ans[j];
+        if (i==n-1) {
+            fout <<pnt[0].x<<" "<<pnt[0].y<<" "<<pnt[n-1].x<<" "<<pnt[n-1].y<<"\n";
+        } else fout <<pnt[i].x<<" "<<pnt[i].y<<" "<<pnt[(i+1)%n].x<<" "<<pnt[(i+1)%n].y<<"\n";
+    }
+    return 0;
+}
