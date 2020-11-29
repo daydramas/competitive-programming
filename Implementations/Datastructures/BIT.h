1013709100742d55b8681c1195eb4e3c186a8ac9
Dong Liu <dongliu0426@gmail.com>
2020-11-28 21:56:34 -0800

    move bit

--- Implementations/Datastructures/BIT.h
+++ Implementations/Datastructures/BIT.h
@@ -1,18 +1,14 @@
 struct BIT {
 
-	int n;
-	vector<int> t;
+	int n; vi t;
 
-	BIT(int n_) {
-		n = n_;
-		t.assign(n+1, 0);
-	}
+	BIT(int n_) { n = n_; t.assign(n+1, 0); }
 
-	void update(int i, int value) {
+	void upd(int i, int value) {
 		for (; i<=n; i+=i&(-i)) t[i] += value;
 	}
 
-	int get(int i) {
+	int qry(int i) {
 		int sum = 0;
 		for(; i>0; i-=i&(-i)) sum += t[i];
 		return sum;
