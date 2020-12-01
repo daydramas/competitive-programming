7fcf1a94ca30acb0c4057f748cc8a79654bdcc8e
Dong Liu <dongliu0426@gmail.com>
2020-12-01 10:59:35 -0800

    edit template

--- Implementations/Contest/template.cpp
+++ Implementations/Contest/template.cpp
@@ -4,32 +4,18 @@
 
 using namespace std;
 using namespace __gnu_pbds;
-template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 using ll = long long;
-using ld = long double;
-using str = string;
-
-using pii = pair<int, int>;
-using pil = pair<int, ll>;
-using pll = pair<ll, ll>;
-
+using pi = pair<int, int>;
 using vi = vector<int>;
 using vl = vector<ll>;
-using vs = vector<str>;
-using vii = vector<pii>;
-template <class T> using v = vector<T>;
 
-#define mp make_pair
 #define f first
 #define s second
-
 #define pb push_back
 #define sz(x) (int)(x).size()
 #define all(x) begin(x), end(x)
-#define sor(x) sort(all(x))
-#define lb lower_bound
-#define ub upper_bound
 
 #define F0R(i,a) for(int i=0; i<(a); i++)
 #define FOR(i,a,b) for(int i=(a); i<=(b); i++)
@@ -37,24 +23,13 @@ template <class T> using v = vector<T>;
 #define ROF(i,a,b) for(int i=(b); i>=a; i--)
 #define trav(a,x) for (auto& a: x)
 
-const int MOD = 1e9+7;
-const ll INF = 1e18;
-const int MX = 2e5+5;
-const ld PI = acos(-1);
-const int X[] = {1, 0, -1, 0},
-          Y[] = {0, 1, 0, -1};
-
-/* ============================ */
-
-void setIO(str PROB = "") {
+void setIO(string PROB = "") {
     cin.tie(0)->sync_with_stdio(0);
     if(sz(PROB)) {
         freopen((PROB+".in").c_str(), "r", stdin);
         freopen((PROB+".out").c_str(), "w", stdout);
     }
 }
-
-/* ============================ */
 
 int main() {
     setIO("$1");
