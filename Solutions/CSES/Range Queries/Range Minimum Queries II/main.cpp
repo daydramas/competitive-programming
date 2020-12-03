b570d0bdb44469b405b31f9da3bcca62f96caf4d
Dong Liu <dongliu0426@gmail.com>
2020-12-03 14:39:43 -0800

    revise sols

--- Solutions/CSES/Range Queries/Range Minimum Queries II/main.cpp
+++ Solutions/CSES/Range Queries/Range Minimum Queries II/main.cpp
@@ -1,134 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef string str;
-
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<ll> vl;
-typedef vector<vl> vvl;
-typedef vector<str> vs;
-
-#define mp make_pair
-#define f first
-#define s second
-#define sz(x) (int)x.size()
-#define all(x) begin(x), end(x)
-#define pb push_back
-
-#define for0(a,n) for(int a = 0; a < (n); ++a)
-#define for1(a,n) for(int a = 1; a <= (n); ++a)
-#define rof0(a,n) for(int a = (n-1); a >= 0; --a)
-#define rof1(a,n) for(int a = (n); a >= 1; --a)
-#define trav(a,x) for (auto& a: x)
-
-const int MOD = 1e9+7; // 998244353;
-const int MX = 2e5+5;
-const ll INF = 1e18;
-const double PI = acos(-1);
-const int X[4] = {1,0,-1,0}, Y[4] = {0,1,0,-1};
-
-/* Source: Benq (https://github.com/bqi343/USACO) [Modified] */
-
-template<class T> bool ckmin(T& a, const T& b) {
-    return b < a ? a = b, 1 : 0; }
-template<class T> bool ckmax(T& a, const T& b) {
-    return a < b ? a = b, 1 : 0; }
-
-namespace IO {
-    void setIO(string PROB = "") {
-        ios_base::sync_with_stdio(0); cin.tie(0);
-        if (sz(PROB)) {
-            freopen((PROB+".in").c_str(),"r",stdin);
-            freopen((PROB+".out").c_str(),"w",stdout);
-        }
-    }
-
-    /* Input */
-    template<class A, class B> void re(pair<A,B>& p);
-    template<class A> void re(vector<A>& v);
-    template<class T> void re(T& x) { cin >> x; }
-    template<class H, class... T> void re(H& h, T&... t) { re(h); re(t...); }
-    template<class A, class B> void re(pair<A,B>& p) { re(p.f,p.s); }
-    template<class A> void re(vector<A>& x) { trav(a,x) re(a); }
-
-    /* Output */
-    template<class A> void pr(A x) { cout << ts(x); }
-    template<class H, class... T> void pr(const H& h, const T&... t) {
-        pr(h); pr(t...); }
-    void pl() { cout << "\n"; }
-}
-using namespace IO;
-
-/* ============================ */
-
-struct segTree {
-
-	int n;
-	vl st;
-
-	segTree(int n_, vl &a) {
-		n = n_;
-		st.assign(4*n, 0);
-		build(1, 1, n, a);
-	}
-
-	int left(int i) { return i*2; }
-	int right(int i) { return i*2+1; }
-
-	void build(int id, int L, int R, vl &a) {
-		if (L == R) {
-			st[id] = a[L-1];
-			return;
+/* SegTree implementation from BenQ */
+template<class T> struct Seg { // comb(ID,b) = b
+	const T ID = 1e18; T comb(T a, T b) { return min(a,b); }
+	int n; vector<T> seg;
+	void init(int _n) { n = _n; seg.assign(2*n,ID); }
+	void pull(int p) { seg[p] = comb(seg[2*p],seg[2*p+1]); }
+	void upd(int p, T val) { // set val at position p
+		seg[p += n] = val; for (p /= 2; p; p /= 2) pull(p); }
+	T query(int l, int r) {	// sum on interval [l, r]
+		T ra = ID, rb = ID;
+		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
+			if (l&1) ra = comb(ra,seg[l++]);
+			if (r&1) rb = comb(seg[--r],rb);
 		}
-		build(left(id), L, (L+R)/2, a);
-		build(right(id), (L+R)/2+1, R, a);
-		st[id] = min(st[left(id)],st[right(id)]);
-	}
-
-	void update(int id, int L, int R, int P, ll value) {
-		if (P > R || P < L) return;
-		if (P >= L && P <= R) st[id]=value;
-		if (L == R) return;
-		update(left(id), L, (R+L)/2, P, value);
-		update(right(id), (R+L)/2+1, R, P, value);
-        st[id] = min(st[left(id)],st[right(id)]);
-	}
-
-	void update(int P, int value) {
-		update(1, 1, n, P, value);
-	}
-
-	ll get(int id, int L, int R, int l, int r) {
-		if (l > R || r < L) return INF;
-		if (l <= L && R <= r) return st[id];
-		return min(get(left(id), L, (R+L)/2, l, r),get(right(id), (R+L)/2+1, R, l, r));
-	}
-
-	ll get(int L, int R) {
-		return get(1, 1, n, L, R);
+		return comb(ra,rb);
 	}
 };
 
+#define ll long long
+Seg<ll> st;
 
 int main() {
-    setIO("");
-
-    int n,q; re(n,q);
-    vl x(n); re(x);
-    vector<pair<int,pii>> Q(q);
-    trav(a, Q) re(a.f, a.s.f,a.s.s);
-    segTree t(n,x);
-    trav(a,Q) {
-        if(a.f==1) {
-            t.update(a.s.f,a.s.s);
-            // x[a.s.f-1]=a.s.s;
-        }
-        else cout << t.get(a.s.f,a.s.s) <<"\n";
-    }
-
+	int n, q; cin >> n >> q;
+	st.init(n);
+	for(int i=1; i<=n; i++) {
+		int a; cin >> a;
+		st.upd(i, a);
+	}
+	for(int i=1; i<=q; i++) {
+		int t, a, b; cin >> t >> a >> b;
+		if (t==1) st.upd(a,b);
+		else cout << st.query(a,b) <<"\n";
+	}
 }
