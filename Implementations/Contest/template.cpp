d9d485d27c2d1c761e426bfc8f21de85d27050e0
Dong Liu <dongliu0426@gmail.com>
2020-12-02 21:53:09 -0800

    shorten template

--- Implementations/Contest/template.cpp
+++ Implementations/Contest/template.cpp
@@ -1,20 +1,11 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
-
 using namespace std;
-using namespace __gnu_pbds;
-template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 using ll = long long;
-using pi = pair<int, int>;
 using vi = vector<int>;
-using vl = vector<ll>;
-
 #define f first
 #define s second
 #define pb push_back
-#define sz(x) (int)(x).size()
 #define all(x) begin(x), end(x)
 
 #define F0R(i,a) for(int i=0; i<(a); i++)
@@ -23,17 +14,9 @@ using vl = vector<ll>;
 #define ROF(i,a,b) for(int i=(b); i>=a; i--)
 #define trav(a,x) for (auto& a: x)
 
-void setIO(string PROB = "") {
-    cin.tie(0)->sync_with_stdio(0);
-    if(sz(PROB)) {
-        freopen((PROB+".in").c_str(), "r", stdin);
-        freopen((PROB+".out").c_str(), "w", stdout);
-    }
-}
 
 int main() {
-    setIO("$1");
 
-    $2
+    $1
 
 }
