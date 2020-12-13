f5f14003fdc3b16b000eb962b9675de92aeff106
Dong Liu <dongliu0426@gmail.com>
2020-12-12 23:28:21 -0800

    add macros

--- Implementations/Contest/pbds.cpp
+++ Implementations/Contest/pbds.cpp
@@ -3,3 +3,8 @@
 using namespace __gnu_pbds;
 
 template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define ins insert
+#define ers erase
+#define oof order_of_key
+#define fbo find_by_order
