ae8b94bea4e1ffe8c75471f861378228fcab7478
Dong Liu <dongliu0426@gmail.com>
2020-12-20 11:30:23 -0800

    upd bit impl.

--- Implementations/Gold/BIT.h
+++ Implementations/Gold/BIT.h
@@ -1,12 +1,6 @@
 const int maxn = 1e5;
 
-using T = int;
-T bit[maxn+5];
-void upd(int i, T value) {
-	for(; i<maxn; i+=i&(-i)) bit[i] += value;
-}
-T qry(int i) {
-	T sum=0;
-	for(; i>0; i-=i&(-i)) sum += bit[i];
-	return sum;
-}
+using T = int; T B[maxn+5];
+void upd(int i, T V) { for(; i<maxn; i+=i&(-i)) B[i]+=V; }
+T qry(int i) { T R=0; for(; i; i-=i&(-i)) R+=B[i];
+	return R; }
