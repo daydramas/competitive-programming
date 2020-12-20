86dea317ed6c8899c5754e1f8eb2303db94a534c
Dong Liu <dongliu0426@gmail.com>
2020-12-20 11:16:42 -0800

    + sol to cowland

--- Solutions/USACO Gold/2018-2019/Feb/cowland/main.cpp
+++ Solutions/USACO Gold/2018-2019/Feb/cowland/main.cpp
@@ -1,180 +1,129 @@
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
-#include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <streambuf>
-#include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
-#include <vector>
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
 using namespace std;
 
-// structures / defines
-#define MAX_N 100000
-#define MAX_D 17
-// global variables
-int N; int Q;
-int E[MAX_N];
-vector<int> edges[MAX_N];
-int tree[MAX_N], depth[MAX_N], lca[MAX_N][MAX_D], label[MAX_N], topchain[MAX_N];
-int segtree[MAX_N*4];
-// functions
-void initLca() {
-	for(int d=1; d<MAX_D; d++) {
-		for(int i=0; i<MAX_N; i++) {
-			lca[i][d] = lca[lca[i][d-1]][d-1];
+using ll = long long;
+using vi = vector<int>;
+#define f first
+#define s second
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+#define F0R(i,a) for(int i=0; i<(a); i++)
+#define FOR(i,a,b) for(int i=(a); i<=(b); i++)
+#define R0F(i,a) for(int i=(a)-1; i>=0; i--)
+#define ROF(i,a,b) for(int i=(b); i>=a; i--)
+#define trav(a,x) for (auto& a: x)
+
+const int maxn = 1e5+5;
+int n, q, v[maxn], v2[maxn], d[maxn], lca[maxn][20];
+vi adj[maxn];
+pair<int,int> itv[maxn]; int t;
+
+
+struct lazySegTree {
+
+	int n; vi st, lz;
+
+	void rsz(int n_) {
+		n = n_;
+		st.assign(4*n, 0);
+		lz.assign(4*n, 0);
+	}
+
+	int left(int i) { return i*2; }
+	int right(int i) { return i*2+1; }
+
+	void propagate(int id, int L, int R) {
+		st[id] ^= lz[id];
+		if (L!=R) {
+			lz[left(id)] ^= lz[id];
+			lz[right(id)] ^= lz[id];
 		}
+		lz[id] = 0;
 	}
-}
-int getLCA(int a, int b) {
-  if(depth[a] < depth[b]) swap(a, b);
-  for(int d = MAX_D-1; d >= 0; d--) {
-    if(depth[a] - (1<<d) >= depth[b]) {
-      a = lca[a][d];
-    }
-  }
-  for(int d = MAX_D-1; d >= 0; d--) {
-    if(lca[a][d] != lca[b][d]) {
-      a = lca[a][d];
-      b = lca[b][d];
-    }
-  }
-  if(a != b) {
-    a = lca[a][0];
-    b = lca[b][0];
-  }
-  return a;
-}
-void setUpSegTree(int idx, int l, int r, int i, int v) {
-	if(l == r) segtree[idx] = v;
-	else {
-		int m = (l+r)/2;
-		if(i <= m) setUpSegTree(2*idx, l, m, i, v);
-		else setUpSegTree(2*idx+1, m+1, r, i, v);
-		segtree[idx] = segtree[idx*2] ^ segtree[idx*2+1];
-	}
-}
-void setUpSegTree(int i, int v) {
-	return setUpSegTree(1, 0, MAX_N-1, i, v);
-}
-void dfsHld(int curr, int topPtr, int par, int& l) {
-	label[curr] = l++;
-	setUpSegTree(label[curr], E[curr]);
-	topchain[curr] = topPtr;
-	int lc = -1, lct = -1;
-	for(int out : edges[curr]) {
-		if(out == par) continue;
-		if(tree[out] > lct) {
-			lc = out, lct = tree[out];
+
+	void upd(int id, int L, int R, int l, int r, int value) {
+		if (lz[id]!=0) propagate(id, L, R);
+		if (l > R || r < L) return;
+		if (l <= L && R <= r) {
+			lz[id] ^= value;
+			propagate(id, L, R);
+			return;
 		}
+		if (L == R) return;
+		upd(left(id), L, (R+L)/2, l, r, value);
+		upd(right(id), (R+L)/2+1, R, l, r, value);
+		st[id] = st[left(id)] ^ st[right(id)];
 	}
-	if(lc < 0 || lct < 0) return;
-	dfsHld(lc, topPtr, curr, l);
-	for(int out : edges[curr]) {
-		if(out == par || out == lc) continue;
-		dfsHld(out, out, curr, l);
+
+	void upd(int L, int R, int value) {
+		upd(1, 1, n, L, R, value);
 	}
-}
-void dfsSize(int curr, int par) {
-	tree[curr]++;
-	for(int out : edges[curr]) {
-		if(out == par) continue;
-		depth[out] = depth[curr]+1;
-		lca[out][0] = curr;
-		dfsSize(out, curr);
-		tree[curr] += tree[out];
+
+	int qry(int id, int L, int R, int l, int r) {
+		if (lz[id]!=0) propagate(id, L, R);
+		if (l > R || r < L) return 0;
+		if (l <= L && R <= r) return st[id];
+		return qry(left(id), L, (R+L)/2, l, r) ^ qry(right(id), (R+L)/2+1, R, l, r);
 	}
-}
-void setUp() {
-	dfsSize(0, -1);
-	initLca();
-	int l = 0;
-	dfsHld(0, 0, -1, l);
-}
-int QsegTree(int idx, int l, int r, int lhs, int rhs) {
-  if(l >= lhs && r <= rhs) return segtree[idx];
-  int ret = 0;
-  int m = (l+r)/2;
-  if(m >= lhs) ret ^= QsegTree(2*idx, l, m, lhs, rhs);
-  if(m+1 <= rhs) ret ^= QsegTree(2*idx+1, m+1, r, lhs, rhs);
-  return ret;
-}
-int QsegTree(int l, int r) {
-	return QsegTree(1, 0, MAX_N-1, l, r);
-}
-int pathQ(int child, int par) {
-	int ans = 0;
-	while(child != par) {
-		if(topchain[child] == child) {
-			ans ^= E[child];
-			child = lca[child][0];
-		} else if (depth[topchain[child]] > depth[par]) {
-			ans ^= QsegTree(label[topchain[child]], label[child]);
-			child = lca[topchain[child]][0];
-		} else {
-			ans ^= QsegTree(label[par]+1, label[child]);
-			break;
-		}
+
+	int qry(int L, int R) {
+		return qry(1, 1, n, L, R);
 	}
-	return ans;
+};
+
+lazySegTree st;
+
+void dfs(int par, int cur, int dep) {
+	v2[cur] = v2[par] ^ v[cur];
+	d[cur]=dep;
+	st.upd(t, t, v2[cur]);
+	itv[cur].f = t++;
+	lca[cur][0]=par;
+	trav(nex, adj[cur]) if(nex!=par) dfs(cur, nex, dep+1);
+	itv[cur].s = t-1;
 }
-int query(int a, int b) {
-	int r = getLCA(a, b);
-	return pathQ(a, r) ^ pathQ(b, r) ^ E[r];
+int jmp(int x, int d) {
+		F0R(i,20) if ((1<<i)&d) x = lca[x][i];
+		return x; }
+int LCA(int x, int y) {
+	if (d[x] < d[y]) swap(x,y);
+	x = jmp(x,d[x]-d[y]); if (x == y) return x;
+	R0F(i,20) {
+		int X = lca[x][i], Y = lca[y][i];
+		if (X != Y) x = X, y = Y;
+	}
+	return lca[x][0];
 }
 int main() {
-	// local variables
 
-	// input
 	ifstream fin("cowland.in");
 	ofstream fout("cowland.out");
 
-	fin >> N >> Q;
-	for(int i=0; i<N; i++) {
-		fin >> E[i];
+	fin >> n >> q;
+	FOR(i,1,n) fin >> v[i];
+	F0R(i,n-1) {
+		int a,b; fin >> a >> b;
+		adj[a].pb(b);
+		adj[b].pb(a);
 	}
-	for(int i=1; i<N; i++) {
-		int a, b; fin >> a >> b;
-		a--; b--;
-		edges[a].push_back(b);
-		edges[b].push_back(a);
-	}
-	setUp();
-	// main
-	for(int i=0; i<Q; i++) {
-		int c,a,b; fin >> c >> a >> b;
-		if(c == 1) {
-			a--; E[a] = b;
-			setUpSegTree(label[a], b);
-		} else {
-			a--; b--;
-			fout << query(a, b) << endl;
+	st.rsz(n+1); t=1, v2[0]=0; dfs(0, 1, 0);
+	FOR(j,1,19) FOR(i,1,n) lca[i][j] = lca[lca[i][j-1]][j-1];
+	F0R(i,q) {
+		int t, a, b; fin >> t >> a >> b;
+		if(t==1) { // upd
+			st.upd(itv[a].f, itv[a].s, v[a]); v[a]=b;
+			st.upd(itv[a].f, itv[a].s, v[a]);
+		} else { // qry
+			if (a==b) { fout << v[a] <<"\n"; continue; }
+			int c = LCA(a,b);
+			// cout <<"lca("<<a<<","<<b<<") = "<<c<<"\n";
+			// cout << st.qry(a,a) <<" ^ "<<st.qry(b,b) <<" ^ "<<st.qry(c,c)<<"\n";
+			fout << (st.qry(itv[a].f,itv[a].f) ^ st.qry(itv[b].f,itv[b].f) ^ v[c]) <<"\n";
 		}
 	}
-	// output
-
-	return 0;
 }
