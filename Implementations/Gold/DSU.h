1943eaf8abd48fd60ef827912e1a80e887ef0945
Dong Liu <dongliu0426@gmail.com>
2020-12-20 17:35:05 -0800

    upd impl.

--- Implementations/Gold/DSU.h
+++ Implementations/Gold/DSU.h
@@ -4,9 +4,6 @@ int D[maxn+5];
 void init() { for(int i=0; i<maxn; i++) D[i]=-1; }
 int par(int x) { return D[x]<0 ? x : D[x]=par(D[x]); }
 bool comb(int x, int y) {
-    x=par(x), x=par(y);
-    if(x==y) return 0;
-    if(D[x] > D[y]) swap(x,y);
-    D[x]+=D[y], D[y]=x;
-    return 1;
+    x=par(x), x=par(y); if(x==y) return 0;
+    D[x]+=D[y], D[y]=x; return 1;
 }
--- Implementations/Gold/SegTree.h
+++ Implementations/Gold/SegTree.h
@@ -3,8 +3,10 @@ const int maxn = 1e5;
 using T = int;
 T st[(maxn+5)*2];
 T ID=0; T comb(T i, T j) { return i+j; }
-void init() { for(int i=0; i<maxn*2; i++) st[i]=ID; }
-void pull(int i) { st[i] = comb(st[i*2], st[i*2+1]); }
+void init() {
+	for(int i=0; i<maxn*2; i++) st[i]=ID; }
+void pull(int i) {
+	st[i] = comb(st[i*2], st[i*2+1]); }
 void upd(int i, T val) {
 	for(st[i+maxn]=val; i>1; i/=2) pull(i/2); }
 T qry(int l, int r) {
