553340edacf5381cb78374c252a6b4eae0fe1f51
Dong Liu <dongliu0426@gmail.com>
2021-01-03 11:26:19 -0800

    BIT

--- Implementations/Datastructures/BIT.h
+++ Implementations/Datastructures/BIT.h
@@ -1,7 +1,9 @@
-const int maxn = 1e5;
-
-using T = int; T B[maxn+5];
-void init() { for(int i=0; i<=maxn; i++) B[i]=0; }
-void upd(int i, T V) { for(; i<maxn; i+=i&(-i)) B[i]+=V; }
-T qry(int i) { T R=0; for(; i; i-=i&(-i)) R+=B[i];
-	return R; }
+template<class T> struct BIT {
+	int N; T *B;
+	BIT() {}
+	BIT(int N) { this->N=N; B = new T[N+1]; }
+	void upd(int i, T V) { for(; i<N; i+=i&(-i)) B[i]+=V; }
+	T qry(int i) { T R=0; for(; i; i-=i&(-i)) R+=B[i];
+		return R; }
+	T qry(int l, int r) { return qry(r)-qry(l-1); }
+};
