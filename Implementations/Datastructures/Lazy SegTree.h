e91fc46d2c7934effe0170e6e576e11686dd6843
Dong Liu <dongliu0426@gmail.com>
2021-01-04 14:48:56 -0800

    fix seg and lazyseg

--- Implementations/Datastructures/Lazy SegTree.h
+++ Implementations/Datastructures/Lazy SegTree.h
@@ -1,21 +1,23 @@
-
 template<class T> struct LSeg {
 	int N; T *st, *lz;
 	T ID=0; T comb(T i, T j) { return i+j; }
+	LSeg() {}
 	LSeg(int N) { this->N = N;
-		st = new T[4*(N+1)]; lz = new T[4*(N+1)] }
+		st = new T[4*(N+1)]; lz = new T[4*(N+1)]; }
 	void pull(int i) {
 		st[i] = comb(st[i*2],st[i*2+1]); }
 	void push(int i, int l, int r) {
 		st[i]+=(r-l+1)*lz[i];
 		if(l!=r) lz[i*2]+=lz[i], lz[i*2+1]+=lz[i];
 		lz[i]=0; }
-	void upd(int L, int R, T v, int i=1, int l=0, int r=maxn) {
+	void upd(int L, int R, T v) { upd(L,R,v,1,1,N); }
+	void upd(int L, int R, T v, int i, int l, int r) {
 		push(i,l,r); if(R<l || L>r) return;
 		if(L<=l && R>=r) { lz[i]=v; push(i,l,r); return; }
 		int m=(l+r)/2; upd(L,R,v,i*2,l,m);
 		upd(L,R,v,i*2+1,m+1,r); pull(i); }
-	T qry(int L, int R, int i=1, int l=0, int r=maxn) {
+	T qry(int L, int R) { return qry(L,R,1,1,N); }
+	T qry(int L, int R, int i, int l, int r) {
 		push(i,l,r); if(R<l || L>r) return ID;
 		if(L<=l && R>=r) return st[i]; int m=(l+r)/2;
 		return comb(qry(L,R,i*2,l,m), qry(L,R,i*2+1,m+1,r)); }
