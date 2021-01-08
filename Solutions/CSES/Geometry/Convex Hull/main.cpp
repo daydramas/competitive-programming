b0bd20eec11d5c588ddc125ad6ab1850f7eb360d
Dong Liu <dongliu0426@gmail.com>
2021-01-08 10:12:30 -0800

    Convex Hull

---
+++ Solutions/CSES/Geometry/Convex Hull/main.cpp
@@ -0,0 +1,105 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pi = pair<int,int>;
+using vi = vector<int>;
+#define f first
+#define s second
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+#define F0R(i,a) for(int i=0; i<(a); i++)
+#define FOR(i,a,b) for(int i=(a); i<=(b); i++)
+#define R0F(i,a) for(int i=(a)-1; i>=0; i--)
+#define ROF(i,a,b) for(int i=(b); i>=a; i--)
+#define trav(a,x) for (auto& a: x)
+
+namespace IO {
+    /* INPUT */
+    char nc() { return getchar_unlocked(); }
+    void rs(string& x) {
+        char ch; x=""; while (isspace(ch = nc()));
+        do { x += ch; } while (!isspace(ch = nc()) && ch != EOF); }
+    template<class T> void ri(T &x) {
+        int sgn=1; char ch; x=0;
+        while (!isdigit(ch = nc())) if (ch == '-') sgn *= -1;
+        x = ch-'0'; while (isdigit(ch = nc())) x = x*10+(ch-'0');
+        x *= sgn; }
+    template<class T, class... Ts> void ri(T& t, Ts&... ts) {
+        ri(t); ri(ts...); }
+    /* OUTPUT */
+    void wc(char ch) { putchar_unlocked(ch); }
+    void ws(string x) { for(char ch : x) wc(ch); }
+    template<class T> inline void wi(T x) {
+        if(x < 0) x*=-1, wc('-');
+        if(0 <= x && x <= 9) wc('0'+x);
+        else wi(x/10), wc('0'+x%10); }
+};
+using namespace IO;
+
+/* source: kactl */
+
+template <class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+};
+typedef Point<long double> P;
+pair<vi, vi> ulHull(const vector<P>& S) {
+	vi Q(sz(S)), U, L;
+	iota(all(Q), 0);
+	sort(all(Q), [&S](int a, int b){ return S[a] < S[b]; });
+	trav(it, Q) {
+#define ADDP(C, cmp) while (sz(C) > 1 && S[C[sz(C)-2]].cross(\
+	S[it], S[C.back()]) cmp 0) C.pop_back(); C.push_back(it);
+		ADDP(U, <=); ADDP(L, >=);
+	}
+	return {U, L};
+}
+
+vi convexHull(const vector<P>& S) {
+	vi u, l; tie(u, l) = ulHull(S);
+	if (sz(S) <= 1) return u;
+	if (S[u[0]] == S[u[1]]) return {0};
+	l.insert(l.end(), u.rbegin()+1, u.rend()-1);
+	return l;
+}
+
+int main() {
+
+    int n; cin >> n; //ri(n);
+    vector<P> a(n), hull;
+    F0R(i,n) cin >> a[i].x >> a[i].y; //ri(a[i].x, a[i].y);
+    trav(i, convexHull(a)) hull.pb(a[i]);
+    // wi(sz(hull)); wc('\n');
+    cout << sz(hull) << '\n';
+    trav(p, hull) {
+        cout << ll(p.x) << ' ' << ll(p.y) << '\n';
+        /*wi(p.x); wc(' '); wi(p.y); wc('\n'); */ }
+
+}
