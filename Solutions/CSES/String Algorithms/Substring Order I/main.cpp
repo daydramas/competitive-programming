98761aeb019b6d71f86ee635c1c5d3bc0b5db282
Dong Liu <dongliu0426@gmail.com>
2021-01-08 10:14:01 -0800

    Substring Order I

---
+++ Solutions/CSES/String Algorithms/Substring Order I/main.cpp
@@ -0,0 +1,108 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pi = pair<int,int>;
+using vi = vector<int>;
+#define f first
+#define s second
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+#define F0R(i,a) for(int i=0; i<(a); i++)
+#define FOR(i,a,b) for(int i=(a); i<=(b); i++)
+#define R0F(i,a) for(int i=(a)-1; i>=0; i--)
+#define ROF(i,a,b) for(int i=(b); i>=a; i--)
+#define trav(a,x) for (auto& a: x)
+
+namespace IO {
+    /* INPUT */
+    char nc() { return getchar_unlocked(); }
+    void rs(string& x) {
+        char ch; x=""; while (isspace(ch = nc()));
+        do { x += ch; } while (!isspace(ch = nc()) && ch != EOF); }
+    template<class T> void ri(T &x) {
+        int sgn=1; char ch; x=0;
+        while (!isdigit(ch = nc())) if (ch == '-') sgn *= -1;
+        x = ch-'0'; while (isdigit(ch = nc())) x = x*10+(ch-'0');
+        x *= sgn; }
+    template<class T, class... Ts> void ri(T& t, Ts&... ts) {
+        ri(t); ri(ts...); }
+    /* OUTPUT */
+    void wc(char ch) { putchar_unlocked(ch); }
+    void ws(string x) { for(char ch : x) wc(ch); }
+    template<class T> inline void wi(T x) {
+        if(x < 0) x*=-1, wc('-');
+        if(0 <= x && x <= 9) wc('0'+x);
+        else wi(x/10), wc('0'+x%10); }
+};
+using namespace IO;
+
+/* source: https://cp-algorithms.com/string/suffix-array.html#toc-tgt-3 */
+vector<int> sort_cyclic_shifts(string const& s) {
+    int n = s.size();
+    const int alphabet = 256;
+    vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
+    for (int i = 0; i < n; i++)
+        cnt[s[i]]++;
+    for (int i = 1; i < alphabet; i++)
+        cnt[i] += cnt[i-1];
+    for (int i = 0; i < n; i++)
+        p[--cnt[s[i]]] = i;
+    c[p[0]] = 0;
+    int classes = 1;
+    for (int i = 1; i < n; i++) {
+        if (s[p[i]] != s[p[i-1]])
+            classes++;
+        c[p[i]] = classes - 1;
+    }
+    vector<int> pn(n), cn(n);
+    for (int h = 0; (1 << h) < n; ++h) {
+        for (int i = 0; i < n; i++) {
+            pn[i] = p[i] - (1 << h);
+            if (pn[i] < 0)
+                pn[i] += n;
+        }
+        fill(cnt.begin(), cnt.begin() + classes, 0);
+        for (int i = 0; i < n; i++)
+            cnt[c[pn[i]]]++;
+        for (int i = 1; i < classes; i++)
+            cnt[i] += cnt[i-1];
+        for (int i = n-1; i >= 0; i--)
+            p[--cnt[c[pn[i]]]] = pn[i];
+        cn[p[0]] = 0;
+        classes = 1;
+        for (int i = 1; i < n; i++) {
+            pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
+            pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
+            if (cur != prev)
+                ++classes;
+            cn[p[i]] = classes - 1;
+        }
+        c.swap(cn);
+    }
+    return p;
+}
+vector<int> suffix_array_construction(string s) {
+    s += "$";
+    vector<int> sorted_shifts = sort_cyclic_shifts(s);
+    sorted_shifts.erase(sorted_shifts.begin());
+    return sorted_shifts;
+}
+
+int main() {
+
+    string s; rs(s);
+    vi a = suffix_array_construction(s);
+    ll x; ri(x);
+    trav(y, a) {
+        int z = sz(s)-y;
+        if(z <= x) x -= z;
+        else { cout <<s.substr(y,x)<<"\n"; break; }
+    }
+
+}
