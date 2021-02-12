9f50c5acf810abe8f5eabd27e947f9d0f82be1ae
Dong Liu <dongliu0426@gmail.com>
2021-02-11 19:08:46 -0800

    Templates

--- Templates/template-long.cpp
+++ Templates/template-long.cpp
@@ -13,6 +13,8 @@ using namespace std;
 using ll = long long;
 using vi = vector<int>;
 using pi = pair<int, int>;
+template<class T> using V = vector<T>;
+template<class T, class U> using PR = pair<T, U>;
 
 #define F           first
 #define S           second
@@ -27,26 +29,29 @@ using pi = pair<int, int>;
 #define EACH(a,x)   for(auto& a: x)
 
 template<class T> bool ckmin(T& a, const T& b) {
-	return (a < b ? a = b, 1 : 0); }
+	return (b < a ? a = b, 1 : 0); }
 template<class T> bool ckmax(T& a, const T& b) {
-	return (a > b ? a = b, 1 : 0); }
+	return (b > a ? a = b, 1 : 0); }
 
 inline namespace IO {
-	template<class T> void read(vector<T>& x);
-	template<class T, class U> void read(pair<T, U>& x);
+	template<class T> void read(V<T>& x);
+	template<class T, class U> void read(PR<T, U>& x);
 	template<class T> void read(T& x) { cin >> x; }
 	template<class T, class... U> void read(T& x, U&... y) { read(x); read(y...); }
-	template<class T> void read(vector<T>& x) { EACH(a, x) read(a); }
-	template<class T, class U> void read(pair<T, U>& x) { read(x.F, x.S); }
-	template<class T> void write(const vector<T>& x);
-	template<class T, class U> void write(const pair<T, U>& x);
+	template<class T> void read(V<T>& x) { EACH(a, x) read(a); }
+	template<class T, class U> void read(PR<T, U>& x) { read(x.F, x.S); }
+	template<class T> void write(const V<T>& x);
+	template<class T, class U> void write(const PR<T, U>& x);
 	template<class T> void write(const T& x) { cout << x; }
 	template<class T, class... U> void write(const T& x, const U&... y) { write(x); write(y...); }
-	template<class T> void write(const vector<T>& x) { bool f=1; EACH(a, x) { if(!f) write(" "); write(a); f=0; } };
-	template<class T, class U> void write(const pair<T, U>& x) { write(x.F, " ", x.S); }
+	template<class T> void write(const V<T>& x) { bool f=1; EACH(a, x) { if(!f) write(" "); write(a); f=0; } };
+	template<class T, class U> void write(const PR<T, U>& x) { write(x.F, " ", x.S); }
 	template<class T> void print(const T& x) { write(x, "\n"); }
 	template<class T, class... U> void print(const T& x, const U&... y) { write(x, " "); print(y...); }
-}; using namespace IO;
+	template<class T> void pdebug(const T& x) { write(x, "]\n"); cout.flush(); }
+	template<class T, class... U> void pdebug(const T& x, const U&... y) { write(x, ", "); pdebug(y...); }
+	#define debug(...) cout << "LINE(" << __LINE__ << ") => [" << #__VA_ARGS__ << "]: [", pdebug(__VA_ARGS__)
+};
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
