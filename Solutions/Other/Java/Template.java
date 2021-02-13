3ccca83d5f12880951be221cbaa9f74f7401d8ad
Dong Liu <dongliu0426@gmail.com>
2021-02-13 11:24:50 -0800

    Clean files, add Java template

---
+++ Solutions/Other/Java/Template.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+	static Scanner r = new Scanner(System.in);
+	public static void main (String[] args)
+	{
+		
+	}
+}
---
+++ Solutions/Other/Java/Template_USACO.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+	static Scanner r = new Scanner(new File("file.in"));
+	static PrintWriter w = new PrintWriter(new FileWriter("file.out"));
+	public static void main (String[] args)
+	{
+		
+	}
+}
