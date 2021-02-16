7ca4bf6c681b12771cfb38925c6d26a797d7d942
Dong Liu <dongliu0426@gmail.com>
2021-02-16 14:54:41 -0800

    Template Changes

--- Templates/temp.cpp
+++ Templates/temp.cpp
@@ -1,35 +1,25 @@
-/*
-============================================================================
- Name:		${1:problem}
- Link:		${2:link}
- Author:	Dong Liu
- Date:		$CURRENT_YEAR-$CURRENT_MONTH-$CURRENT_DATE
-============================================================================
-*/
+/**
+ * $1
+ * Created on $CURRENT_YEAR-$CURRENT_MONTH-$CURRENT_DATE
+ */
 
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-using ll = long long;
-using vi = vector<int>;
-using pi = pair<int, int>;
+#define N (1 << 18) // 2e5
 
-#define F           first
-#define S           second
-#define PB          push_back
-#define ALL(x)      begin(x), end(x)
-#define SZ(x)       int((x).size())
-#define FOR(i,a,b)  for(int i=(a); i<=(b); ++i)
-#define F0R(i,x)    FOR(i,0,(x)-1)
-#define ROF(i,a,b)  for(int i=(b); i>=(a); --i)
-#define R0F(i,x)    ROF(i,0,(x)-1)
-#define REP(x)      F0R(_,x)
-#define EACH(a,x)   for(auto& a: x)
-
+template <class T> void read(T &x) { cin >> x; }
+template <class T, class... U> void read(T &x, U &...y) { read(x), read(y...); }
+template <class T> void write(const T &x) { cout << x; }
+template <class T, class... U> void write(T x, U... y) { write(x, y...); }
+template <class T> void print(T x) { write(x, '\n'); }
+template <class T, class... U> void print(T x, U... y) { write(x, ' '), print(y...); }
 
 int main() {
-    cin.tie(0)->sync_with_stdio(0);
+	cin.tie(0)->sync_with_stdio(0);
 
-    $0
-
+	$0
 }
--- Templates/ts.cpp
+++ Templates/ts.cpp
@@ -1,8 +1,12 @@
-/*
- *
+/**
+ * $1
+ * Created on $CURRENT_YEAR-$CURRENT_MONTH-$CURRENT_DATE
  */
 
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 #define N (1 << 18) // 2e5
