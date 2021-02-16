f03ef144188bfbfb9bf3223f3058835fc606f137
Dong Liu <dongliu0426@gmail.com>
2021-02-16 14:48:01 -0800

    Format files

--- Solutions/Old (Eclipse)/Codeforces/BinaryPeriod/src/BinaryPeriod.cpp
+++ Solutions/Old (Eclipse)/Codeforces/BinaryPeriod/src/BinaryPeriod.cpp
@@ -2,29 +2,32 @@
 
 using namespace std;
 
-//structures/defines
+// structures/defines
 #define ll long long
 
-//global variables
+// global variables
 
-//functions
+// functions
 char getop(char ch) {
-	if (ch == '1') return '0';
+	if (ch == '1')
+		return '0';
 	return '1';
 }
 void solve() {
-	string str; cin >> str;
+	string str;
+	cin >> str;
 	bool same = true;
-	for (int i=1; i<str.size(); i++) {
-		if (str[i]!=str[i-1]) same = false;
+	for (int i = 1; i < str.size(); i++) {
+		if (str[i] != str[i - 1])
+			same = false;
 	}
 	if (same) {
 		cout << str << endl;
 	} else {
 		string ans = "";
 		ans += str[0];
-		for (int i=1; i<str.size(); i++) {
-			if (str[i] == str[i-1]) {
+		for (int i = 1; i < str.size(); i++) {
+			if (str[i] == str[i - 1]) {
 				ans += getop(str[i]);
 			}
 			ans += str[i];
@@ -33,14 +36,16 @@ void solve() {
 	}
 }
 int main() {
-	//local variables
+	// local variables
 
-	//input
-	int t; cin >> t;
-	for (int i=0; i<t; i++) solve();
-	//main
+	// input
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+		solve();
+	// main
 
-	//output
+	// output
 
 	return 0;
 }
--- Solutions/USACO Training/Chapter 2/2.4 cowtour.cpp
+++ Solutions/USACO Training/Chapter 2/2.4 cowtour.cpp
@@ -1,4 +1,4 @@
-  
+
 /*
  ID: dongliu3
  TASK: cowtour
@@ -22,9 +22,9 @@ double ans;
 
 // functions
 double getD(int i, int j) {
-    double dx = x[i]-x[j];
-    double dy = y[i]-y[j];
-    return sqrt(dx*dx + dy*dy);
+	double dx = x[i] - x[j];
+	double dy = y[i] - y[j];
+	return sqrt(dx * dx + dy * dy);
 }
 int main() {
 	// local variables
@@ -35,28 +35,38 @@ int main() {
 
 	// input
 	fin >> N;
-	for (int i=1; i<=N; i++) fin >> x[i] >> y[i];
-	for (int i=1; i<=N; i++) {
-		string str; fin >> str;
-		for (int j=1; j<=N; j++) {
-			if (str[j-1] == '1') d[i][j] = getD(i, j);
-			else d[i][j] = INF;
-			if (i==j) d[i][j] = 0;
+	for (int i = 1; i <= N; i++)
+		fin >> x[i] >> y[i];
+	for (int i = 1; i <= N; i++) {
+		string str;
+		fin >> str;
+		for (int j = 1; j <= N; j++) {
+			if (str[j - 1] == '1')
+				d[i][j] = getD(i, j);
+			else
+				d[i][j] = INF;
+			if (i == j)
+				d[i][j] = 0;
 		}
 	}
 	// main
 	ans = INF;
-	for (int k=1; k<=N; k++) for (int i=1; i<=N; i++) for (int j=1; j<=N; j++)
-		if (d[i][k]+d[k][j] < d[i][j])
-			d[i][j] = d[i][k]+d[k][j];
-	for (int i=1; i<=N; i++) {
+	for (int k = 1; k <= N; k++)
+		for (int i = 1; i <= N; i++)
+			for (int j = 1; j <= N; j++)
+				if (d[i][k] + d[k][j] < d[i][j])
+					d[i][j] = d[i][k] + d[k][j];
+	for (int i = 1; i <= N; i++) {
 		mD[i] = -1;
-		for (int j=1; j<=N; j++) if (d[i][j] != INF)
-			mD[i] = max(mD[i], d[i][j]);
-		for (int j=1; j<i; j++) if (d[i][j] == INF)
-			ans = min(ans, mD[i]+mD[j]+getD(i, j));
+		for (int j = 1; j <= N; j++)
+			if (d[i][j] != INF)
+				mD[i] = max(mD[i], d[i][j]);
+		for (int j = 1; j < i; j++)
+			if (d[i][j] == INF)
+				ans = min(ans, mD[i] + mD[j] + getD(i, j));
 	}
-	for (int i=1; i<=N; i++) ans = max(ans, mD[i]);
+	for (int i = 1; i <= N; i++)
+		ans = max(ans, mD[i]);
 	// output
 	fout << fixed << setprecision(6) << ans << endl;
 	return 0;
