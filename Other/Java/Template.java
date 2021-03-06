75c76e2fa039d37174f7b220e9d360ee98aa052d
Dong Liu <dongliu0426@gmail.com>
2021-03-06 09:20:00 -0800

    Update gitignore

--- Other/Java/Template.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-import java.util.*;
-
-class Main {
-	public static void main (String[] args) throws IOException {
-		Scanner r = new Scanner(System.in);
-	}
-}
--- Other/Java/Template_USACO.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.util.*;
-
-class Main {
-	public static void main (String[] args) throws IOException {
-		Scanner r = new Scanner(new File("file.in"));
-		PrintWriter w = new PrintWriter(new FileWriter("file.out"));
-	}
-}
