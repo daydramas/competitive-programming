9411de06e9585f17d64d8de379efb1f26b3209f5
Dong Liu <dongliu0426@gmail.com>
2021-03-06 09:20:03 -0800

    template

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <utility>
+#include <vector>
+using namespace std;
+
+#define MOD	1000000007
+#define INF	1e9
+#define EPS	1e-9
+#define N	100000
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	
+}
