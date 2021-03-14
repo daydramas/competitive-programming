4acd2de3afd3f8d8da3f2fd1ef427bce9d587570
Dong Liu <dongliu0426@gmail.com>
2021-03-14 13:39:40 -0700

    CSES Exponentiation I

---
+++ USACO/USACO Guide/gold/modular/cses-1095.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <utility>
+#include <vector>
+using namespace std;
+
+#define mem(a, x)                memset(a, x, sizeof(a))
+#define FOR(i, a, b)    for (int i = (a); i <= (b); ++i)
+#define F0R(i, x)          for (int i = 0; i < (x); ++i)
+#define ROF(i, a, b)    for (int i = (b); i >= (a); --i)
+#define R0F(i, x)     for (int i = (x) - 1; i >= 0; --i)
+#define each(x, a)                     for (auto& x : a)
+
+template <class T> bool ckmin(T& a, const T& b) { return (a > b ? a = b, 1 : 0); }
+template <class T> bool ckmax(T& a, const T& b) { return (a < b ? a = b, 1 : 0); }
+template <class T> void read(T &x) { cin >> x; }
+template <class T, class... U> void read(T &x, U &...y) { read(x); read(y...); }
+template <class T> void write(const T &x) { cout << x; }
+template <class T, class... U> void write(const T &x, const U &...y) { write(x); write(y...); }
+template <class T> void print(const T &x) { write(x, "\n"); }
+template <class T, class... U> void print(const T &x, const U &...y) { write(x, " "); print(y...); }
+template <class T> void DBG(const T& t) { write(t, "]", '\n'); cout.flush(); }
+template <class T, class... U> void DBG(const T& t, const U&... u) { write(t, ", "); DBG(u...); }
+#define error(...) cerr << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+
+
+const long long MOD = 1000000007;
+const int INF = 1000000000;
+const int N   = 100005;
+
+long long pow(long long a, long long b) {
+	if (b == 0)
+		return 1;
+	return pow(a * a % MOD, b / 2)
+		 * (b & 1 ? a : 1) % MOD;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	
+	int n;
+	long long a, b;
+	read(n);
+	F0R(i, n) {
+		read(a, b);
+		print(pow(a, b));
+	}
+}
