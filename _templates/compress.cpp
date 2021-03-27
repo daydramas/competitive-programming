57e4b18af570026c11145d1b5ea026ce8f090aa5
Dong Liu <dongliu0426@gmail.com>
2021-03-26 18:56:48 -0700

    Update compress

--- _templates/compress.cpp
+++ _templates/compress.cpp
@@ -5,21 +5,21 @@ struct D {
 	int p; // position
 } aa[N];
 
-int b[N]; // new, reordered
+int a[N]; // new, reordered
 
 for (int i = 0; i < n; ++i) {
 	cin >> aa[i].x;
 	aa[i].p = i;
 }
 
-sort(p, p + n, [](int i, int j) {
-	return aa[i].x < aa[j].x; // compare
+sort(aa, aa + n, [](const D &i, const D &j) {
+	return i.x < j.x; // compare
 });
 
-b[aa[0].p] = 0;
+a[aa[0].p] = 0;
 for (int i = 1; i < n; ++i) {
-	b[aa[i].p] = b[aa[i - 1].p];
+	a[aa[i].p] = a[aa[i - 1].p];
 	if (aa[i].x != aa[i - 1].x) // diff element
-		b[aa[i].p]++;
+		a[aa[i].p]++;
 }
 
